df2d428487526cd77c14e22de905290ab979b143
Tom Reeve <moosereeve@gmail.com>
2015-02-24 08:08:52 -0500

    ShreKit readded as submodule

---
+++ PantherNews/HHSSHKConfigurator.h
@@ -0,0 +1,13 @@
+//
+//  HHSSHKConfigurator.h
+//  PantherNews
+//
+//  Created by Thomas Reeve on 2/24/15.
+//  Copyright (c) 2015 Holliston High School. All rights reserved.
+//
+
+#import "DefaultSHKConfigurator.h"
+
+@interface HHSSHKConfigurator : DefaultSHKConfigurator
+
+@end
---
+++ PantherNews/HHSSHKConfigurator.m
@@ -0,0 +1,13 @@
+//
+//  HHSSHKConfigurator.m
+//  PantherNews
+//
+//  Created by Thomas Reeve on 2/24/15.
+//  Copyright (c) 2015 Holliston High School. All rights reserved.
+//
+
+#import "HHSSHKConfigurator.h"
+
+@implementation HHSSHKConfigurator
+
+@end
